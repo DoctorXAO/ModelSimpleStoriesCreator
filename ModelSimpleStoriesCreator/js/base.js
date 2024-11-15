2f890170130e88c381730e35f0580a0b5aeb4b93
XAOTI <122676114+DoctorXAO@users.noreply.github.com>
2024-11-15 14:13:55 +0200

    Delete ModelSimpleStoriesCreator directory

--- ModelSimpleStoriesCreator/js/base.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-function showHiddenPanel() {
-	const hiddenPanel = document.getElementById("hiddenPanel");
-	
-	hiddenPanel.classList.toggle('active');
-};

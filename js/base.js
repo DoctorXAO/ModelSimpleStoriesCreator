34d28d464154deaa55ae9184505992a875db3ea1
sergii <tdocsg00@gmail.com>
2024-11-16 11:30:21 +0200

    create-a-story.html created; The mistake with the account logo fixed

--- js/base.js
+++ js/base.js
@@ -3,3 +3,7 @@ document.getElementById("year").textContent = new Date().getFullYear();
 function showHiddenPanel() {	
 	document.querySelector(".wrapper").classList.toggle('open-side-bar');
 };
+
+function openPage(link) {
+	window.open(link, '_self');
+};
